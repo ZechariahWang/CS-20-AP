144fdf3e84d40d1c1241096ceabe002603d0c20d
Zechariah Wang <Zechariahwang@gmail.com>
2022-09-16 23:38:15 -0600

    Array Intro Finished

--- ArrayMenu.java
+++ ArrayMenu.java
@@ -4,10 +4,11 @@ import java.util.Random; // Importing random utilities
 
 class Main {
 
-  static int globalArray[] = new int[99];
+  static int globalArray[] = new int[100]; // Global array. Altered by user in main method
+  static Boolean Exit = false; // Exit condition. If made true, will exit program
 
-  static Boolean Exit = false;
 
+  // This condition checks for user input and returns it to the main method
   public static int ConditionChecker(){
     Scanner UserInput = new Scanner(System.in);
     int value = 0;
@@ -32,38 +33,60 @@ class Main {
   }
   
   public static void populate_sequentially(){ // Fill array with an increment each time???
-      System.out.println("populating sequen");
-  }
-  
-  public static void display(){ // Display array data???
-    System.out.println("displaying");
-    for (int i = 0; i < globalArray.length; i++){
-       System.out.print(globalArray[i] + ", ");
-    }
-    System.out.print("array length: " + globalArray.length);
-  }
-  
-  public static void shuffle(){ // Display array data???
-    int index, temp;
+    System.out.println("populating sequen");
     Random random = new Random();
-    
-    for (int i = globalArray.length - 1; i > 0; i--)
-    {
-        index = random.nextInt(i + 1);
-        temp = globalArray[index];
-        globalArray[index] = globalArray[i];
-        globalArray[i] = temp;
+    int init_count = 0;
+    for (int i = 0; i <= 99; i++){
+      init_count += 1;
+      globalArray[i] = i + 1;
     }
   }
 
+  public static void display(){ // Display array data???
+    System.out.println("displaying");
+    for (int i = 1; i < 101; i++){
+      if (i % 5 == 0 && i != 0){
+        System.out.println(globalArray[i - 1]);    
+      }
+      else{
+        System.out.print(globalArray[i - 1] + " ");  
+      }
+
+    }
+  }
+  
+  public static void shuffle(){ // shuffle array data????
+    Random random = new Random();
+    for (int i = globalArray.length - 1; i > 0; i--){
+      int index = random.nextInt(i + 1);
+      int a = globalArray[index];
+      globalArray[index] = globalArray[i];
+      globalArray[i] = a;
+    }
+  }
+
+  // This method exits out of the program
   public static void ExitProgram(){
     System.out.println("Exiting");
     Exit = true;
   }
-  
+
+  // This program displays prompts for the user to enter commands into the main method
+  public static void DisplayPrompt(){
+    System.out.println(" ");
+    System.out.println("1 = populate randomly");
+    System.out.println("2 = populate sequentially");
+    System.out.println("3 = shuffle");
+    System.out.println("4 = display");
+    System.out.println("5 = exit");
+    System.out.println("Enter Desired command:");
+  }
+
+
+  // PLEASE I WANNA DIE
   public static void main(String[] args) {
     do{ 
-      System.out.println("Desired command: ");
+      DisplayPrompt();
       int decision = ConditionChecker();
       if (decision == 1){
         populate_rand();
@@ -72,10 +95,10 @@ class Main {
         populate_sequentially();
       }
       else if (decision == 3){
-        display();
+        shuffle();
       }
       else if (decision == 4){
-        shuffle();
+        display();
       }
       else if (decision == 5){
         ExitProgram();
