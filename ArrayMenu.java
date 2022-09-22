b3f4d5f8d9aa21eba4a4555c4767d746f1bc8020
Zechariah Wang <Zechariahwang@gmail.com>
2022-09-21 20:47:37 -0600

    Added more commands

--- ArrayMenu.java
+++ ArrayMenu.java
@@ -12,7 +12,6 @@ class Main {
   public static int ConditionChecker(){
     Scanner UserInput = new Scanner(System.in);
     int value = 0;
-
     try{
       value = UserInput.nextInt();
     }
@@ -20,6 +19,43 @@ class Main {
       System.out.println("invalid input");
     }
 
+    return value;
+  }
+
+  // This condition checks for user input and returns it to the main method
+  public static int InputNum(){
+    int value = 0;
+    boolean invalid = false;
+    do{
+      System.out.println("Enter number: ");
+      Scanner UserInput = new Scanner(System.in);
+      try{
+        value = UserInput.nextInt();
+        invalid = true;
+      }
+      catch (Exception e){
+        System.out.println("invalid input (1)");
+      }
+    } while (invalid == false);
+    
+    return value;
+  }
+
+  public static int InputReplacementNum(){
+    int value = 0;
+    boolean invalid = false;
+    do{
+      System.out.println("Enter replacement number: ");
+      Scanner UserInput = new Scanner(System.in);
+      try{
+        value = UserInput.nextInt();
+        invalid = true;
+      }
+      catch (Exception e){
+        System.out.println("invalid input (1)");
+      }
+    } while (invalid == false);
+    
     return value;
   }
   
@@ -43,7 +79,7 @@ class Main {
   }
 
   public static void display(){ // Display array data???
-    System.out.println("displaying");
+    System.out.println("displaying...");
     for (int i = 1; i < 101; i++){
       if (i % 5 == 0 && i != 0){
         System.out.println(globalArray[i - 1]);    
@@ -63,6 +99,83 @@ class Main {
       globalArray[index] = globalArray[i];
       globalArray[i] = a;
     }
+    System.out.println("Shuffled randomly!");
+  }
+
+  public static void findFirstOccurance(int num){ // Find first occurance of a number in the array
+    for (int i = 0; i < globalArray.length; i ++){
+      if (num == globalArray[i]){
+        System.out.println("The first occurance of " + num + " is at index: " + i);
+        return;
+      }
+    }
+    System.out.println("No number in list! (Error code -1)");
+  }
+
+  public static void checkAscending(){ // Check if array is in ascending order
+    for (int i = 0; i < globalArray.length - 1; i++) {
+      if (globalArray[i] > globalArray[i + 1]) {
+        System.out.println("Array is not in ascending order!");
+        return;
+      }
+    }
+    System.out.println("Array is in ascending order!");
+  }
+
+  public static void findLowestValue(){ // Find lowest number in array
+    int minValue = globalArray[0];
+    for(int i = 1; i < globalArray.length; i++){
+      if(globalArray[i] < minValue){
+        minValue = globalArray[i];
+      }
+    }
+    System.out.println("Lowest number in array is: " + minValue);
+  }
+
+  public static void findHighestValue(){ // Find highest number in array
+    int maxValue = globalArray[0];
+    for(int i = 1; i < globalArray.length; i++){
+      if(globalArray[i] > maxValue){
+        maxValue = globalArray[i];
+      }
+    }
+      
+    System.out.println("Highest number in array is: " + maxValue);
+  }
+
+  public static void numberOccurance(int num){ // Find occurance of num
+    int res = 0;
+    for (int i = 0; i < globalArray.length; i++){
+        if (num == globalArray[i]){
+          res++;
+        }
+    }
+  
+    System.out.println("Occurance: " + res + " time(s)");
+  }
+
+  public static void replaceNum(int num, int replacementNum){ // Find occurance of num
+    int replacementCounter = 0;
+    for (int i = 0; i < globalArray.length; i++){
+        if (globalArray[i] == num){
+          globalArray[i] = replacementNum;
+          replacementCounter ++;
+        }
+    }
+    System.out.println("Replaced on " + replacementCounter + " occasions!");
+  }
+
+  public static void sortArray(){ // Bubble sort algorithm
+    for (int i = 0; i < globalArray.length - 1; i++){
+      for (int j = 0; j <  globalArray.length - i - 1; j++){
+        if (globalArray[j] > globalArray[j + 1]){
+          int temp = globalArray[j];
+          globalArray[j] = globalArray[j + 1];
+          globalArray[j + 1] = temp;
+        }
+      }
+    }
+    System.out.println("Sorted!");
   }
 
   // This method exits out of the program
@@ -78,10 +191,16 @@ class Main {
     System.out.println("2 = populate sequentially");
     System.out.println("3 = shuffle");
     System.out.println("4 = display");
-    System.out.println("5 = exit");
+    System.out.println("5 = Find First occurance");
+    System.out.println("6 = Check if ascending");
+    System.out.println("7 = Find lowest value");
+    System.out.println("8 = Find highest value");
+    System.out.println("9 = Find number Occurance");
+    System.out.println("10 = Replace Number");
+    System.out.println("11 = Sort Array");
+    System.out.println("12 = exit");
     System.out.println("Enter Desired command:");
   }
-
 
   // PLEASE I WANNA DIE
   public static void main(String[] args) {
@@ -101,10 +220,35 @@ class Main {
         display();
       }
       else if (decision == 5){
+        int chosenNum = InputNum();
+        findFirstOccurance(chosenNum);
+      }
+      else if (decision == 6){
+        checkAscending();
+      }
+      else if (decision == 7){
+        findLowestValue();
+      }
+      else if (decision == 8){
+        findHighestValue();
+      }
+      else if (decision == 9){
+        int chosenNum = InputNum();
+        numberOccurance(chosenNum);
+      }
+      else if (decision == 10){
+        int chosenNum = InputNum();
+        int replacementNum = InputReplacementNum();
+        replaceNum(chosenNum, replacementNum);
+      }
+      else if (decision == 11){
+        sortArray();
+      }
+      else if (decision == 12){
         ExitProgram();
       }
       else{
-        System.out.println("Invalid num");
+        // System.out.println("Invalid num");
       }
     } while (Exit == false);
     
